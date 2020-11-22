0fce989fbe3cbbd8aa973f3b8edec58222ae10f8
sapphirehead <djey36@gmail.com>
2020-11-22 22:04:06 +0000

    Update index.js

--- public_html/result4/index.js
+++ public_html/result4/index.js
@@ -1,41 +1,30 @@
-const { createServer } = require('http');
-// создайте REPL  с клиентом
-// отправляющим тело запроса
+const http = require("http");
 
-createServer((req, res) => {
-  const ip = res.socket.remoteAddress;
-  console.log(req.url);
-  console.log('ip', ip);
-  let CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-    'Access-Control-Allow-Headers':
-      'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
-  };
-  const result = {
-    message: 'djey36',
-    'x-result': req.headers['x-test'],
-    'x-body': '',
-  };
-  if (req.url === '/result4/') {
-    let body = '';
+http
+  .Server((req, res) => {
 
-    req
-      .on('data', (data) => {
-        body += data;
-        result['x-body'] = body;
-        return result;
-      })
-      .on('end', () => {
-        res.writeHead(200, {
-          ...CORS,
-          'Content-Type': 'application/json',
+    console.log(req.url)
+    if (req.url === "/result4/") {
+      let CORS = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+        "Access-Control-Allow-Headers":
+          "x-test,Content-Type,Accept,Access-Control-Allow-Headers",
+      };
+
+      const result = {
+        message: "djey36",
+        "x-result": req.headers["x-test"],
+      };
+      let body = "";
+
+      req
+        .on("data", (data) => (body += data))
+        .on("end", () => {
+          result["x-body"] = body;
+          res.writeHead(200, {... CORS, "Content-Type": "application/json" });
+          res.end(JSON.stringify(result));
         });
-        res.end(JSON.stringify(result));
-      });
-  } else {
-    res.writeHead(200, { ...CORS, 'Content-Type': 'application/json' });
-
-    res.end(JSON.stringify(result));
-  }
-}).listen(3000, () => console.log('working on 3000'));
+    }
+  })
+  .listen(4321, () => console.log("Server Ok"));
